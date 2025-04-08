2a69b19d1b00591819f1d24990412fa4ec334c34
rus1980707 <rus1980707@gmail.com>
2025-04-08 17:43:00 +0300

    data-jpa  changed

--- src/test/java/web/intro/WebIntroApplicationTests.java
+++ src/test/java/web/intro/WebIntroApplicationTests.java
@@ -2,8 +2,10 @@ package web.intro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WebIntroApplicationTests {
 
     @Test
